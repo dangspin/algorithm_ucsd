44c5b736fbe9d6a9ee6747603a529e80dec507f2
dangspin <dangxiaoqian@gmail.com>
2016-03-25 16:22:07 -0500

    Update max_pairwise_product.py

--- section1/week1/max_pairwise_product.py
+++ section1/week1/max_pairwise_product.py
@@ -1,13 +1,26 @@
-# Uses python2
-n = int(input())
-a = [int(x) for x in input().split()]
-assert(len(a) == n)
+## This work calculate the maximum pairwise product for a given array, which has elements bigger than 
+## two. It is allowed to have identical elements in the array.
+
+## raw input and convert it to list
+n = int(raw_input())
+data = [int(x) for x in raw_input().split()]
+assert(len(data)==n)
 
 result = 0
 
-for i in range(0, n):
-    for j in range(i+1, n):
-        if a[i]*a[j] > result:
-            result = a[i]*a[j]
+## the maximum value
+max_index=0
+for i in range(len(data)):
+    if data[i]>=data[max_index]:
+        max_index=i
+
+## the second maximum value
+max_index2=max_index-1
+for j in range(len(data)):
+    if j!=max_index and data[j]>=data[max_index2]:
+        max_index2=j
+
+## pairwise product
+result=data[max_index]*data[max_index2]
 
 print result
